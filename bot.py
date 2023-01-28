0cee7d38122b6af9aa397918404d553e3eff7e96
comradexlight <comradexlight@gmail.com>
2023-01-28 18:52:21 +0300

    updated util

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher,  types
 from settings import API_TOKEN
-from utils import roll_dice, from_image_to_bytes
-from make_img import print_number, make_facet_img
+from utils import print_number
 
 
 logging.basicConfig(level=logging.INFO)
@@ -18,18 +17,17 @@ dice_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
     types.KeyboardButton(text="Кинуть d12"),
     types.KeyboardButton(text="Кинуть d20"))
 
-@dp.message_handler(regexp="Кинуть d\d*")
+@dp.message_handler(regexp="Кинуть d\\d*")
 async def send_roll(message: types.Message):
-   dice = int(message.text[8:]) 
-   rolled_number = roll_dice(dice)
-   number_image = print_number(rolled_number)
-   # number_image = make_facet_img(rolled_number)
-   img_to_send = from_image_to_bytes(number_image)
-   print(message.from_user.id, message.from_user.first_name)
-   await message.answer_photo(img_to_send, reply_markup=dice_keyboard)
+    dice = int(message.text[8:])
+    logging.log(level=logging.INFO,
+                msg=f"{message.from_user.first_name} rolled dice: {dice}")
+    img_to_send = print_number(dice)
+    await message.answer_photo(img_to_send, reply_markup=dice_keyboard)
 
 
 @dp.message_handler(commands=["start", "help"])
 @dp.message_handler()
 async def send_welcome(message: types.Message):
-   await message.answer("Я бот для броска костей.\n", reply_markup=dice_keyboard)
+    await message.answer("Я бот для броска костей.\n", reply_markup=dice_keyboard)
+
--- utils.py
+++ utils.py
@@ -1,16 +1,30 @@
 from io import BytesIO
 from random import randint
-from PIL.Image import Image as ImageClass 
+from PIL.Image import Image as ImageClass
+from PIL import Image, ImageDraw, ImageFont
 
 
-def roll_dice(dice: int) -> int:
+def _roll_dice(dice: int) -> int:
     return randint(1, dice)
 
 
-def from_image_to_bytes(img: ImageClass) -> bytes:
+def _from_image_to_bytes(img: ImageClass) -> bytes:
     img_byte_arr = BytesIO()
-    img.save(img_byte_arr, format="JPEG")
+    img.save(img_byte_arr, format="PNG")
     return img_byte_arr.getvalue()
-   
-# def from_image_to_bytes(img: ImageClass) -> bytes:
-    # return img.tobytes("hex","rgb")
+
+
+def _make_image(number: int) -> ImageClass: 
+    img = Image.open("src/images/template.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("src/fonts/Alice-Regular.ttf", 120)
+    draw.text((150, 150), text=f"{number}",
+              anchor="mm", fill="black", font=font)
+    return img
+
+
+def print_number(dice: int) -> bytes:
+    number = _roll_dice(dice)
+    img = _make_image(number)
+    return _from_image_to_bytes(img)
+
